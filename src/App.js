5c35467abcad6f16979185416fcadf4109aa1cf0
Volodymyr Melnyk <melnykvova90@gmail.com>
2019-06-17 16:39:36 +0300

    delete files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +15,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>{' '}
       </header>
     </div>
   );
